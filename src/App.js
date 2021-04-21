d103b4d57ac2f1f18b5eee94a866240c065f4deb
Da Lu <daniel.lu@rently.com>
2021-04-21 09:51:48 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -22,6 +22,7 @@ class Board extends React.Component {
   render() {
     return (
       <div>
+        <h1>Tic Tac Toe</h1>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
